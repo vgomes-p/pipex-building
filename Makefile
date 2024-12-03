3b247341d953e220ed937e5dcb8a8b269769fea2
vgomes-p <vinied.personal@gmail.com>
2024-12-02 22:18:37 -0300

    commit_00

---
+++ Makefile
@@ -0,0 +1,49 @@
+NAME		=	pipex
+CC			=	gcc
+FLAGS		=	-Wall -Wextra -Werror -I $(H_DIR) -I $(LIBFT_DIR)
+RM			=	rm -rf
+
+# Caminhos e arquivos
+H_SRCS		=	pipex.h
+H_DIR		=	includes/
+HEADER		=	$(addprefix $(H_DIR), $(H_SRCS))
+
+MAPA_SRCS	=	pipex.c ppx_utils.c
+MAPA_DIR	=	pipexcodes/
+MAPA		=	$(addprefix $(MAPA_DIR), $(MAPA_SRCS))
+OBJ_M		=	$(MAPA:.c=.o)
+
+LIBFT_DIR	=	libft/
+LIBFT		=	$(LIBFT_DIR)libft.a
+
+# Regras de compilação
+%.o: %.c $(HEADER) Makefile
+			$(CC) $(FLAGS) -c $< -o $@
+
+$(NAME): $(OBJ_M) $(LIBFT)
+			$(CC) $(FLAGS) $(OBJ_M) $(LIBFT) -o $(NAME)
+			@echo -e "$(GREEN)$(NAME) created!$(DEFAULT)"
+
+$(LIBFT):
+			@make -C $(LIBFT_DIR)
+
+all:		$(NAME)
+
+clean:
+			@$(RM) $(OBJ_M)
+			@make clean -C $(LIBFT_DIR)
+			@echo -e "$(YELLOW)object files deleted!$(DEFAULT)"
+
+fclean: clean
+			@$(RM) $(NAME)
+			@make fclean -C $(LIBFT_DIR)
+			@echo -e "$(RED)all deleted!$(DEFAULT)"
+
+re:			fclean all
+
+.PHONY:		all clean fclean re
+
+RED  = \033[1;31m
+GREEN = \033[1;32m
+YELLOW = \033[1;33m
+DEFAULT = \033[0m
---
+++ includes/pipex.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vgomes-p <vgomes-p@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/02 22:07:08 by vgomes-p          #+#    #+#             */
+/*   Updated: 2024/12/02 22:07:08 by vgomes-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+/* INCLUDES */
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <stdio.h>
+# include <sys/wait.h>
+# include <fcntl.h>
+# include "../libft/libft.h"
+
+/* ppx_utils.c functions */
+void	error(void);
+char	*find_path(char *cmd, char **envp);
+int		get_next_line(char **line);
+void	execute(char *argv, char **envp);
+
+#endif
